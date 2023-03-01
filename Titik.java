3be58e5942e7de49f3e2aa7a216edbb5ce70d55b
IhsanAlif <100286370+IhsanAlif@users.noreply.github.com>
2023-03-01 19:51:03 +0700

    Add files via upload

---
+++ Titik.java
@@ -0,0 +1,34 @@
+//Nama : Ihsan Alif Lazuardi Anwar
+//NIM  : 24060121140162
+//Lab  : PBO C1
+
+class Titik{
+	private double absis;
+	private double ordinat;
+	static int counterTitik; //menghitung objek titik
+	public Titik(double a, double o){
+		counterTitik++;
+		absis = a;
+		ordinat = o;
+	}
+	public Titik(){
+		counterTitik++;
+		absis = 0;
+		ordinat = 0;
+	}
+	public void setAbsis(double a){
+		absis = a;
+	}
+	public void setOrdinat(double o){
+		ordinat = o;
+	}
+	double getAbsis(){
+		return absis;
+	}
+	double getOrdinat(){
+		return ordinat;
+	}
+	int getCounterTitik(){
+		return counterTitik;
+	}
+}
